a050e33b7416de4b212072d34fcd92cad2d85ad3
Kim G <97060659+kg-byte@users.noreply.github.com>
2022-05-16 19:19:26 -0600

    Update github_service_spec.rb

--- spec/github_service_spec.rb
+++ spec/github_service_spec.rb
@@ -9,12 +9,4 @@ RSpec.describe GithubService do
   	expect(response[:login]).to eq('kg-byte')
   end
 
-  # it 'returns response' do 
-  # 	user_data = JSON.parse(File.read('lib/user_response.json'), symbolize_names: true)
-  # 	allow(GithubService).to receive(:get_user).and_return(user_data)
-
-  #   response = GithubService.get_user
-  # 	expect(response).to have_key(:login)
-  # 	expect(response[:login]).to eq('kg-byte')
-  # end 
 end
