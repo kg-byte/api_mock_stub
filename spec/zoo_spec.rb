d97985a877c02f0e6dbb97878c35fd0e6d298bee
Kim G <xiaole.guo@yahoo.com>
2022-05-16 19:12:18 -0600

    starting point

--- spec/zoo_spec.rb
+++ spec/zoo_spec.rb
@@ -25,17 +25,6 @@ RSpec.describe Zoo do
  	expect(zoo.animal_names).to eq(['Lucy', 'Tino'])
  end
 
- it 'can add animals' do 
- 	cat = double('Lucy the cat')
- 	dog = double('Tino the dog')
 
- 	allow(cat).to receive(:name).and_return('Lucy')
- 	allow(dog).to receive(:name).and_return('Tino')
-
- 	zoo.add_animal(cat)
- 	zoo.add_animal(dog)
-
- 	expect(zoo.animal_names).to eq(['Lucy', 'Tino'])
- end
 
 end
